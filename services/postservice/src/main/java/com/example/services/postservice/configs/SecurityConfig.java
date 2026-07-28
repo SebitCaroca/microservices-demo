51c099feb32135304fcbeaf8903447bcc57e339d
SebitCaroca <sebastian.caroca@usach.com>
2026-07-28 07:39:00 -0400

    opened up the ports i accidentally forgot to open

--- services/postservice/src/main/java/com/example/services/postservice/configs/SecurityConfig.java
+++ services/postservice/src/main/java/com/example/services/postservice/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.services.postservice.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
