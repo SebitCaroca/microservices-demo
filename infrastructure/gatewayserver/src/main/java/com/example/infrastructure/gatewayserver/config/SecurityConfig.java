7955a3b5657cdefe666bff96e4e4924978805df8
SebitCaroca <sebastian.caroca@usach.com>
2026-07-28 12:03:36 -0400

    attempt to fix gateway

--- infrastructure/gatewayserver/src/main/java/com/example/infrastructure/gatewayserver/config/SecurityConfig.java
+++ infrastructure/gatewayserver/src/main/java/com/example/infrastructure/gatewayserver/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,16 +15,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        PathPatternRequestMatcher.Builder matcherBuilder = PathPatternRequestMatcher.withDefaults();
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                HttpMethod.GET,
-                                "/api/v1/posts",
-                                "/api/v1/posts/**",
-                                "/api/v1/profiles",
-                                "/api/v1/profiles/**"
-                                ).permitAll()
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/actuator/health")).permitAll()
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/actuator/health/**")).permitAll()
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/api/v1/posts")).permitAll()
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/api/v1/posts/**")).permitAll()
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/api/v1/profiles")).permitAll()
+                        .requestMatchers(matcherBuilder.matcher(HttpMethod.GET, "/api/v1/profiles/**")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
