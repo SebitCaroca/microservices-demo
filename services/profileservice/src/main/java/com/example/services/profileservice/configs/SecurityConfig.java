df4ad8febddf8b7654ef11b9824f647a68f91e26
SebitCaroca <sebastian.caroca@usach.com>
2026-07-28 10:56:31 -0400

    opening up more ports for gateway and etc

--- services/profileservice/src/main/java/com/example/services/profileservice/configs/SecurityConfig.java
+++ services/profileservice/src/main/java/com/example/services/profileservice/configs/SecurityConfig.java
@@ -19,7 +19,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/api/v1/profiles/**").permitAll()
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/api/v1/profiles",
+                                "/api/v1/profiles/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
