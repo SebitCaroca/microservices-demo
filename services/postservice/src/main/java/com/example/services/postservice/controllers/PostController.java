a3cc3e813aed72bb9a22477e586e18e21cce42fa
SebitCaroca <sebastian.caroca@usach.com>
2026-07-28 01:08:54 -0400

    i forgot some stuff sorry

--- services/postservice/src/main/java/com/example/services/postservice/controllers/PostController.java
+++ services/postservice/src/main/java/com/example/services/postservice/controllers/PostController.java
@@ -6,7 +6,6 @@ import com.example.services.postservice.services.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
